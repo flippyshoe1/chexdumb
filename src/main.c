1aa3e5a24f20f8d28f7d2a38eb6a8588d367c6ff
flippyshoe <flippyshoe@debian-desktop-snes.lan>
2024-05-14 11:49:53 +0300

    fixed bug where program would print the leftover data at the buffer, even if inaccurate

--- src/main.c
+++ src/main.c
@@ -12,6 +12,9 @@
  * - split the hex print to a seperate function, write my own custom hex printer and not use printf for fun :3
  */
 
+/* limits */
+#define CYCLECAP 10
+
 /* quick and dirty assert macro to save writing */
 #define ASSERT(_e, ...) if(!(_e)) { fprintf(stderr, __VA_ARGS__); exit(1); }
 
@@ -26,16 +29,24 @@ int main(int argc, char **argv){
   ASSERT((fd=open(path, 0)) > 0,
 	 "Failed to open file, err: %s\n", strerror(errno));
 
-  /* iterate over every 128 bytes, print the data of each byte in hex */
-  uint8_t buff[128];
-  int c=0;
-  while(read(fd, &buff, 128)>0){
-    for(int i=0;i<128;i++, c++){
-      if(i%10==0){
-	printf("\n%.6X | ", c/12);
-      }
-      
+  /* iterate over every 10 bytes, print the data of each byte in hex */
+  uint8_t buff[CYCLECAP];
+  int c=0; // the line we are currently at
+  int r;   // the amount of bytes read by read()
+  while((r=read(fd, &buff, CYCLECAP))>0){
+    /* print the line in hex because lol */
+    printf("\n%.6X | ", c/12);
+
+    /* print the raw data, every x columns specified by CYCLECAP (or until reaching EOF) */
+    for(int i=0;i<r;i++, c++){
       printf("%.2X ", buff[i]);
+    }
+
+    /* clean buffer by setting all the values to 0
+     * TODO: i need a better method to clean up the buffer
+     */
+    for(int i=0;i<CYCLECAP;i++){
+      buff[i]=0;
     }
   }
   
